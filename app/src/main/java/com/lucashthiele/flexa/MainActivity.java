ed411d791dc0d5895db064d4f19cf83007529c46
lucashthiele <lucasthiele0@outlook.com>
2024-07-09 22:53:32 -0300

    init

---
+++ app/src/main/java/com/lucashthiele/flexa/MainActivity.java
@@ -0,0 +1,27 @@
+package com.lucashthiele.flexa;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.app_bar);
+        toolbar.setTitle("");
+
+        setSupportActionBar(toolbar);
+    }
+
+}
